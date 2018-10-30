c56d3530b08cdd7cc4d75427a082b288397bb65f
Andrius PEKO <hello@peko.lt>
2018-10-30 13:33:51 +0200

    35

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,14 +3,14 @@ import Layout from "../components/layout"
 
 import Skills from "../components/skills"
 
-const About = ({ data }) => (
-<Layout>
-  <section className="hero">
-    <h1>Welcome to “One man Show”</h1>
-    <p>I love working with entrepreneurs and being part of the exciting journey of growing a business. It’s fun, but it can also be tough.</p>
-  </section>
-  <Skills />
-</Layout>
+const About = () => (
+  <Layout>
+    <section className="hero">
+      <h1>Welcome to “One man Show”</h1>
+      <p>I love working with entrepreneurs and being part of the exciting journey of growing a business. It’s fun, but it can also be tough.</p>
+    </section>
+    <Skills />
+  </Layout>
 )
 
 export default About
