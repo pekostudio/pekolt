59750dfec5cbb2abae2567c2edb234929cbb2b7e
Andrius PEKO <hello@peko.lt>
2018-10-30 12:06:03 +0200

    31

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,27 +2,15 @@ import React from 'react'
 import Layout from "../components/layout"
 import { graphql } from 'gatsby'
 
-const About = ({ data }) => (
+import Skills from "../components/skills"
 
+const About = ({ data }) => (
 <Layout>
   <section className="hero">
     <h1>Welcome to “One man Show”</h1>
     <p>I love working with entrepreneurs and being part of the exciting journey of growing a business. It’s fun, but it can also be tough.</p>
   </section>
-  <section className="skills">
-    <div className="skillsLeft">
-      <img src="https://images.ctfassets.net/58wyapl352h9/4zCqdKpGYo4kgYSECk8am2/97ed48170ef10b609ecbf15fe87df9e8/noun_581614.png" Width="150" Height="150" alt="" />
-      <p>Design <br /><br /> UX, User Experience Design, UI, User Interface design, Web application design, Websites, Mobile design, iOS & Android platforms, Unique Design Systems, Graphic design, Creative, Team Lead, Wireframes, Prototypes.</p>
-    </div>
-    <div className="skillsCenter">
-      <img src="https://images.ctfassets.net/58wyapl352h9/7mneM4FRlYeoosW0eiIqE4/3d6e29c64ee64a120d0a3a87da630914/noun_568069-300x300.png" Width="150" Height="150" alt="" />
-      <p>Development <br /><br /> Php, HTML5, CSS3, JavaScript, AJAX, XML, jQuery, React, VueJs, Webpack, Gatsby, Jekyll, JAMstack sites, Bootstrap, Foundation, Headless CMS, Strapi, Netlify, Contentful and others. Knowledge ‘fundamentals of server side coding’.</p>
-    </div>
-    <div className="skillsRight">
-      <img src="https://images.ctfassets.net/58wyapl352h9/3cHbhLFUdyu8CS6wm26iSG/50d108e5f1bca775e1b875e6692b5c92/noun_646983-300x300.png" Width="150" Height="150" alt="" />
-      <p>Support 24/7 <br /><br /> As you grow, i’ll be with you every step of the way, making your life easier, and offering great advice (just when you need it) to help you achieve your dreams.</p>
-    </div>
-  </section>
+  <Skills />
 </Layout>
 )
 
