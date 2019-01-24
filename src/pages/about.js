056b937981aff2500b7180cbe7c360431e8a6e09
Andrius PEKO <hello@peko.lt>
2019-01-24 12:31:22 +0200

    50

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,8 +7,10 @@ const About = ({ data }) => (
   <Layout>
     <section className="hero">
       <h1>Welcome to “One Man Show”</h1>
-      <p>I love working with entrepreneurs and being part of the exciting journey of growing a business. <i>Delivering Exceptional Service.</i> Exceptional client service is about going beyond what is realistically expected of you. It is about surprising, and often delighting, customers, turning them into enthusiastic referral sources and lifelong clients who stick with you not only because you do great work at a fair price, but because the value you bring to them goes far beyond just your products.        
+      <p>I love working with entrepreneurs and being part of the exciting journey of growing a business. <i>Delivering Exceptional Service.</i> Exceptional client service is about going beyond what is realistically expected of you.        
       </p>
+      <p>It is about surprising, and often delighting, customers, turning them into enthusiastic referral sources and lifelong clients who stick with you not only because you do great work at a fair price, but because the value you bring to them goes far beyond just your products.        
+      </p>      
     </section>
     {data.allContentfulSkills.edges.map((edge, id) => (
     <Skills key={id} node={edge.node} />
